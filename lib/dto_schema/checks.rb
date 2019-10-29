ed2fceeb24f1bf1609c7e873c8a41f10f577fa3b
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-29 20:56:43 +0700

    Write comments

--- lib/dto_schema/checks.rb
+++ lib/dto_schema/checks.rb
@@ -1,11 +1,13 @@
 module DTOSchema
   module Checks
 
+    # A custom DTO-attribute validation
     class Check
       def initialize (check)
         @check = check
       end
 
+      # Validate attribute value
       def validate (data, args = nil)
         result = @check.call(data) if args.nil?
         result = @check.call(data, args) unless args.nil?
@@ -14,47 +16,59 @@ module DTOSchema
         []
       end
 
+      # Ensure check doesn't have dangling references
       def resolve
-        self
+        self # simple Check never has references
       end
     end
 
+    # A dynamic reference to a custom Check
     class CheckReference
       def initialize (schema, ref, args = nil)
         @schema, @ref = schema, ref
         @args = args
       end
 
+      # Validate attribute value
       def validate (data, args = nil)
         resolve.validate data, args
       end
 
+      # Get the referent check
       def resolve
         @schema.resolve_check @ref
       end
     end
 
+    # A validation check with bound key-word arguments.
+    #
     class BoundCheck
       def initialize (check, args)
         @check = check
         @args = args
       end
 
+      # Validate attribute value
       def validate (data, args = {})
         args = @args.merge(args)
-        @check.validate data, args
+        @check.validate data, args # pass predefined kw-args
       end
 
+      # Ensure the underlying check doesn't have dangling references
       def resolve
         @check.resolve
       end
     end
 
+    # It provides check-binding API:
+    #   check.length(min: 2, max: 3)
     class CheckBinder
       def initialize (schema)
         @schema = schema
       end
 
+      # Bind a check by its name passed as dynamic method name.
+      # It is this method that implements check-binding API in schema definition.
       def method_missing (name, args = {})
         check = CheckReference.new @schema, name
         BoundCheck.new check, args
